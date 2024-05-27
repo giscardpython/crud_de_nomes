d68e15283fc9513288a1ffcfb893ba90cb7ea291
Giscard Stephanou <giscard.silva@aluno.senai.br>
2024-05-27 19:14:58 -0300

    Versão 1.0

--- crud_de_nomes.py
+++ crud_de_nomes.py
@@ -1,7 +1,12 @@
+import os
+
 escolha = 1
+opcao = 0
+
+nomes_lista = []
 
 while escolha > 0:
-    print ("Lista de Opções\n")
+    print ("\nLista de Opções: \n")
     print('1 - Inserir pessoa na Lista')
     print('2 - Listar pessoas cadastradas')
     print('3 - Pesquisar pelo nome de uma pessoa')
@@ -10,46 +15,57 @@ while escolha > 0:
     print('6 - Deletar nome da lista')
     print('7 - Sair do programa')
 
-    opcoes = int(input('\nEscolha a sala desejada (1 a 7):\n'))
+    opcao1 = int(input('\nEscolha a opção desejada (1 a 7):\n'))
     
-    match opcoes:
+    match opcao1:
         case 1:
-                nomes_lista = []
-                while True:
-                    try:    
-                        novo_nome = input('Informe o novo nome a ser inserido na lista: ')
-                        if novo_nome !='':
-                           nomes_lista.append(novo_nome)
-                           print(f'{novo_nome} inserido com sucesso.')
-                           continue
-                        else: 
-                            break
-                    except:
-                        print('Não foi possível inserir o nome.')
+            while True:
+                novo_nome = input('Informe o novo nome a ser inserido na lista: ')
+                if novo_nome !='':
+                    nomes_lista.append(novo_nome)
+                    print(f'{novo_nome} inserido com sucesso.')
+                    continue
+                else: 
+                    os.system('cls')
+                    for nome in nomes_lista:
+                        print(nome)                
+                    break
         case 2:
             for nome in nomes_lista:
                 print(nome)
         case 3:
-            nome = input('Informe o nome que deseja encontrar: ')
+            nome = input('Informe o nome que deseja encontrar: ').capitalize()
             # busca o nome desejado
-            if nome in nomes_lista:
-                print(nome)
-            else:
-                print(f'{nome} não encontrado.')
+            try:
+                indice = nomes_lista.index(nome)
+                quantidade = nomes_lista.count(nome)
+                print(f'Nome encontrado: {nome} {quantidade} vez(es) no índice {indice}')
+            except:
+                print(f'{nome} não encontrado na lista.')
         case 4:
             nomes_lista.sort()        
             # lista ordenada...
             print ('Lista ordenada:')
             for nome in nomes_lista:
                 print(nome)
+        case 5:
+            indice2 = int(input('Informe o índice que deseja alterar: '))
+            indice2 -= 1
+            nomes_lista[indice2] = input('Informe o novo nome: ')
+            for nome in nomes_lista:
+                print(nome)        
+        case 6:
+            nome_a_excluir = input('Informe o nome que deseja retirar da lista: ')
+            try:
+                nomes_lista.remove(nome_a_excluir)
+            except:
+                print('Nome não pode ser excluído.')
+
+            for nome in nomes_lista:
+                print(nome)        
         case 7:
             escolha = 0
             break
-
-
-
-
-
-
-
-
+        case _:
+            print('Opção inváida! Digite uma opção de 1 a 7')
+            continue      
